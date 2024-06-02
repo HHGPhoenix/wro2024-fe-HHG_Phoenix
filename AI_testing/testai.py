8e5b69147c79dc38b8a67a71205dbad37834dd62
Emil Deßloch <emildess0303@gymhelm.onmicrosoft.com>
2024-06-02 13:36:33 +0200

    refactor: Remove commented out code and update .gitignore

--- AI_testing/testai.py
+++ AI_testing/testai.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model, Model
 
